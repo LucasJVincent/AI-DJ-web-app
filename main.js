1e00696b5820c1a764ce4ecb467185929755d346
LucasJVincent <102920397+LucasJVincent@users.noreply.github.com>
2022-12-11 17:36:33 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song= "";
+leftWristX = 0;
+leftWristY = 0;
+righttWristX = 0;
+rightWristY = 0;
 
 function preload(){
     song = loadSound("JoJo's Bizarre Adventure Opening 1 Full『SONO CHI NO SADAME』.webm")
@@ -10,14 +14,37 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function gotPoses(results) {
+    if(results.length > 0) 
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x
+        leftWristY = results[0].pose.leftWrist.y
+        console.log("leftWristX = "+ leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x
+        rightWristY = results[0].pose.rightWrist.y
+        console.log("rightWristX = "+ rightWristX +" rightWristY = "+ rightWristY);
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Activated')
 }
 
 function draw() {
-    Image(video, 0, 0, 600, 500);
+    image(video, 0, 0, 600, 500);
 }
 
 function play() {
     song.play()
+    song.setVolume(1);
+    song.rate(1);
 }
 
 function stop() {
